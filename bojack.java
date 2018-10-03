34b501b38ef39d0c56e9e31d413e1811cd7c6eb7
Lucas Silveira <lucassilveira@dcc.ufmg.br>
2018-10-03 18:57:35 -0300

    WIP: Add movement functions

--- bojack.java
+++ bojack.java
@@ -1,9 +1,10 @@
 import java.util.*;
 import java.lang.*;
 
+
 public class bojack {
 
-  public static int[][] move = {{2,1}, {1,2}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {1,-2}, {-2,-1}};
+  public static int[][] ValidMoves = {{2,1}, {1,2}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {1,-2}, {-2,-1}};
 
   public static void main(String[] args)
   {
@@ -29,7 +30,34 @@ public class bojack {
 
     // << inicio classe PrintBoard recebe board e tamanho
     board = new int[size][size];
+    MoveValue = new int [2];
+
+    int moveNumber = 1;
+    int[] positionZ = {0,0};
     
+
+    
+    Random rand = new Random();
+    int NextMove = rand.nextInt(8);
+
+    MoveValue[0] = positionZ[0] + ValidMoves[NextMove][0];
+    MoveValue[1] = positionZ[1] + ValidMoves[NextMove][1];
+
+    if ( MoveValue[0] >= 0 &&
+         MoveValue[0] < 8 &&
+         MoveValue[1] >= 0 &&
+         MoveValue[1] < 8 &&
+         board[positionZ[0]][positionZ[1]] < 1)
+      {
+        
+
+      }
+
+    ;
+    positionZ[1] += ValidMoves[NextMove][1];
+
+    board[positionZ[0]][positionZ[1]] = moveNumber;
+
     for(int i = 0; i < size; i++)
     {
       for(int j = 0; j < size; j++)
@@ -43,5 +71,20 @@ public class bojack {
     }
     System.out.println("\nOK");
     // << fim da classe PrintBoard, retorna 0.
+
+
+
+/*
+
+
+    //>> make a move:
+
+    
+
+*/
+
+
+
+
   }
 }
